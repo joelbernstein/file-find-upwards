6a2f8c505ca58a10a78f2aaef5927bd45d8e6c8c
Marcel Gruenauer <hanekomu@gmail.com>
2008-07-25 14:07:29 +0000

    continued to update distribution to standard style
    git-svn-id: http://svn.coderepos.org/share/lang/perl/File-Find-Upwards/trunk@16479 d0d07461-0603-4401-acd4-de1884942a52

--- lib/File/Find/Upwards.pm
+++ lib/File/Find/Upwards.pm
@@ -42,15 +42,15 @@ sub file_find_upwards :Memoize {
 
 __END__
 
-{% USE p = PodGenerated %}
+
 
 =head1 NAME
 
-{% p.package %} - FIXME
+File::Find::Upwards - FIXME
 
 =head1 SYNOPSIS
 
-    {% p.package %}->new;
+    File::Find::Upwards->new;
 
 =head1 DESCRIPTION
 
@@ -58,13 +58,45 @@ __END__
 
 =over 4
 
-{% p.write_methods %}
+
 
 =back
 
-{% p.write_inheritance %}
+File::Find::Upwards inherits from L<Exporter>.
 
-{% PROCESS standard_pod %}
+The superclass L<Exporter> defines these methods and functions:
+
+    as_heavy(), export(), export_fail(), export_ok_tags(), export_tags(),
+    export_to_level(), import(), require_version()
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHORS
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 by the authors.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
 =cut
 
